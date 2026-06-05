66c2dafd0aa83c07788d4af3de0422edd26b80a8
Alkan0 <alkinoos.m@outlook.com>
2026-06-05 20:27:57 +0300

    refactor: use typed HttpClient for products API

--- src/CSharpApp.Infrastructure/Configuration/HttpConfiguration.cs
+++ src/CSharpApp.Infrastructure/Configuration/HttpConfiguration.cs
@@ -4,6 +4,15 @@ public static class HttpConfiguration
 {
     public static IServiceCollection AddHttpConfiguration(this IServiceCollection services)
     {
+        var serviceProvider = services.BuildServiceProvider();
+        var configuration = serviceProvider.GetService<IConfiguration>();
+        var restApiSettings = configuration!.GetSection(nameof(RestApiSettings)).Get<RestApiSettings>();
+
+        services.AddHttpClient<IProductsService, ProductsService>(client =>
+        {
+            client.BaseAddress = new Uri(restApiSettings!.BaseUrl!);
+        });
+
         return services;
     }
 }
